cad664f3813ae7c69e1b7acd8ec3d4b411673b34
Brent Lewis <brent.lewis08@gmail.com>
2016-08-12 18:33:56 -0500

    init

---
+++ js-src/component.js
@@ -0,0 +1,7 @@
+var component = {
+    doSomething: function () {
+        console.log('componentizing synergy modules');
+    }
+};
+
+export default component;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './js-src/entry.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
